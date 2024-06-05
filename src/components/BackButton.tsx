fa643a08736212ccddbf94d76fc67f1f05c27335
veelcheck <veelcheck@gmail.com>
2024-06-05 13:50:32 +0200

    styling

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 function BackButton() {
   const navigate = useNavigate();
 
-  const goBack = () => navigate(-1);
+  const goBack = () => navigate('/');
   return (
     <div className='px-2 text-amber-50 border rounded-md'>
       <button
